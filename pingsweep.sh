6dcc43e428da9d82d64da2bf6ca46d4d79601d43
tannerklinge <tannerklinge@u.boisestate.edu>
2026-02-02 11:34:14 -0700

    main outlined

--- pingsweep.sh
+++ pingsweep.sh
@@ -3,6 +3,24 @@
 
 base="onyxnode"
 
+main(){
+	if [ $# -eq 0 ]; then
+		cmd_up
+		exit 0
+	fi
+
+for cmd in "$@"; do
+	case "$cmd" in
+	help|h) cmd_help;;
+	*)
+	echo "Unknown command: $cmd"
+	cmd_help
+	exit 1
+	;;
+   esac
+done
+	}
+
 for q in {1..200}
 do
 	curr="$base$q"
